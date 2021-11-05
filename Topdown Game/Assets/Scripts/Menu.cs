b0a9e9c2750f3e0e72b9cd963289870553f72386
Henry Owen <hnryowen1@gmail.com>
2021-11-05 09:09:24 -0700

    Instructions section of main menu

--- Topdown Game/Assets/Scripts/Menu.cs
+++ Topdown Game/Assets/Scripts/Menu.cs
@@ -2,11 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     public AudioClip startSound;
     public int SFXdelay = 3;
+    public Canvas instructionCanvas;
+    public Canvas mainCanvas;
+
+    private void Start()
+    {
+        instructionCanvas.enabled = false;
+        mainCanvas.enabled = true;
+    }
+
+    public void HowToPlay()
+    {
+        instructionCanvas.enabled = true;
+        mainCanvas.enabled = false;
+    }
+
+    public void DisableInstructions()
+    {
+        instructionCanvas.enabled = false;
+        mainCanvas.enabled = true;
+    }
 
     public void LoadMainMenu()
     {
